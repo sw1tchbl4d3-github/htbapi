f4c192606cb2e56214fc7729c7da158098fc8d52
sw1tch-bl4d3 <jsimeze@pm.me>
2020-02-08 17:36:05 +0100

    Modified setup.py and added Pycharm to gitignore

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-        name = "htb-api",
-        version = "1.0",
-        desription = "An unofficial API Wrapper for Hackthebox (and more!)",
-        long_description = open("README.md", "r").read(),
-        license = "GPL",
-        author = "sw1tchbl4d3",
-        author_email = "jsimeze@pm.me",
-        url = "https://github.com/sw1tchbl4d3-github/htb-api",
-        packages = ["htb-api"],
-        install_requires = []
+        name="htb-api",
+        version="1.0",
+        desription="An unofficial API Wrapper for Hackthebox (and more!)",
+        long_description=open("README.md", "r").read(),
+        license="GPL",
+        author="sw1tchbl4d3",
+        author_email="jsimeze@pm.me",
+        url="https://github.com/sw1tchbl4d3-github/htb-api",
+        packages=["htb-api"],
+        install_requires=[]
 )
