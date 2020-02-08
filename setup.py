c28c7eff2f243b88307816433f2c168bfb631f2d
sw1tchbl4d3-github <jsimeze@pm.me>
2020-02-09 00:22:22 +0100

    htb-api to htbapi

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-        name="htb-api",
+        name="htbapi",
         version="1.0",
         desription="An unofficial API Wrapper for Hackthebox (and more!)",
         long_description=open("README.md", "r").read(),
         license="GPL",
         author="sw1tchbl4d3",
         author_email="jsimeze@pm.me",
-        url="https://github.com/sw1tchbl4d3-github/htb-api",
-        packages=["htb-api"],
+        url="https://github.com/sw1tchbl4d3-github/htbapi",
+        packages=["htbapi"],
         install_requires=[]
 )
