49239a250e34565cd30f922d4005ead7cd9a2742
sw1tch-bl4d3 <jsimeze@pm.me>
2020-02-11 12:30:37 +0100

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup
 
 setup(
         name="htbapi",
-        version="1.0",
-        description="An unofficial API Wrapper for Hackthebox (and more!)",
+        version="1.1",
+        description="An unofficial API Wrapper for Hackthebox.",
         long_description=open("README.md", "r").read(),
+        long_description_content_type="text/markdown",
         license="GPL",
         author="sw1tchbl4d3",
         author_email="jsimeze@pm.me",
